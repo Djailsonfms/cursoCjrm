810beb031d7ca128e7b75d059758d6d3a6ed7141
Djailson F M Silva <djailsonfms@gmail.com>
2022-09-24 13:54:11 -0300

    Aula 2,3,4,5,6

--- aula 01/app.js
+++ aula 01/app.js
@@ -1,3 +1,13 @@
-alert(
-    "Hello World!"
-);
+// Comentário de uma linha
+/*
+Comentario de multiplas linhas
+*/
+let age = 21;
+let currentYear = 2019;
+console.log(age, currentYear);
+
+age = 32;
+console.log(age);
+
+const points = 100;
+console.log(points);
